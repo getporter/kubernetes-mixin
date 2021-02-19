bd5170a49893deba10365a9a82f6ae3011c2af84
Carolyn Van Slyck <me@carolynvanslyck.com>
2021-02-19 15:29:10 -0600

    Use shared publishing logic
    Porter now has a shared library that mixins can use to share common
    build logic. It just has PublishMixin and PublishMixinFeed right now and
    we can incrementally move more targets into mage as we go.
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

---
+++ magefile.go
@@ -0,0 +1,8 @@
+// +build mage
+
+package main
+
+// mage:import
+import _ "get.porter.sh/porter/mage/releases"
+
+// We are migrating to mage, but for now keep using make as the main build script interface.
